315e21d20178af84a2c899057e1021387c45e10d
Joyce Tao <jtao@lcs.on.ca>
2021-01-25 09:43:48 -0500

    arrows

--- Playground.playground/Pages/Velvet Underground Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Velvet Underground Poster.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
  */
-let preferredWidth = 600
+let preferredWidth = 400
 let preferredHeight = 600
 /*:
  ## Required code
@@ -35,10 +35,30 @@ PlaygroundPage.current.liveView = canvas
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
 
  */
+//Colors
+let black=Color(hue:0,saturation:0,brightness:0,alpha:100)
+let purple=Color(hue:309,saturation:78,brightness:64,alpha:100)
+let offWhite=Color(hue:83,saturation:4,brightness:89,alpha:100)
+canvas.drawAxes(withScale:true,by:50)
 
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
-canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
-
+///canvas.fillColor=black
+///canvas.drawRectangle(at:Point(x:0,y:0),width:400,height:600)
+///for ppl in stride(from:10,through:290,by:70){
+func arrow(relativeTo p: Point, color: Color) {
+    canvas.defaultLineWidth=17
+    canvas.lineColor=color
+    canvas.drawLine(from:Point(x:p.x,y:p.y),to:Point(x:p.x,y:p.y+48))
+    canvas.drawLine(from:Point(x:p.x,y:p.y),to:Point(x:p.x+48,y:p.y))
+    canvas.drawLine(from:Point(x:p.x+5,y:p.y+5),to:Point(x:p.x+60,y:p.y+60))
+}
+for allp in stride(from:8,through:328,by:80){
+    for ppl in stride(from:8,through:328,by:80){
+        arrow(relativeTo:Point(x:ppl,y:allp),color: purple)
+    }
+}
+for wte in stride(from:8,through:328,by:80){
+    arrow(relativeTo:Point(x:wte,y:wte),color: offWhite)
+}
 /*:
  ## Show the Live View
  Don't see any results?
